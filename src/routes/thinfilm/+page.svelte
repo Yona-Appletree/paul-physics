428bc066c319d7003b19d3d0a6e0e56a7737ebd1
Yona Appletree <hypher@gmail.com>
2025-06-24 17:57:17 -0700

    Initial commit

---
+++ src/routes/thinfilm/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	//
+	// Physics simulation of a single ray of light hitting a thin film on a
+	// substrate, used to demonstrate the constructive and destructive interference
+	// for introductory-level physics classes
+	//
+
+	// Non-adjustable parameters
+	const wavelengthNm = 500;
+	const airN = 1;
+	const substrateN = 1.5; // index of refraction
+
+	// User adjustable parameters
+	const filmChoices = [
+		{
+			label: 'MgF2',
+			n: 1.38
+		},
+		{
+			label: 'SiO2',
+			n: 1.65
+		}
+	];
+	const filmChoiceIndex = 0;
+	const angleDegrees = 15; // angle from vertical
+</script>
+
+<svelte:head>
+	<title>Thin film simulation</title>
+	<meta
+		name="description"
+		content="A simulation of light interacting with a thin film on a substrate"
+	/>
+</svelte:head>
